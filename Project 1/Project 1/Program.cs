9ebb70162914da37ace52fdb699cb2f98cc33d89
gnohgnij <congjinghong@gmail.com>
2021-09-05 23:29:39 +0800

    Create Record

---
+++ Project 1/Project 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Record r = new Record("abc", 4, 100);
+            Record a = new Record("def", 5, 50);
+            Console.WriteLine("Size of r = " + Marshal.SizeOf(r));
+            Console.WriteLine("Size of a = " + Marshal.SizeOf(a));
+        }
+    }
+}
---
+++ Project 1/Project 1/Record.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Project_1
+{
+    struct Record
+    {
+        private string tconst;
+        private int averageRating;
+        private int numVotes;
+        
+        public Record(string tconst, int averageRating, int numVotes)
+        {
+            this.tconst = tconst;
+            this.averageRating = averageRating;
+            this.numVotes = numVotes;
+        }
+
+        public string getTConst()
+        {
+            return this.tconst;
+        }
+
+        public int getAverageRating()
+        {
+            return this.averageRating;
+        }
+
+        public int getNumVotes()
+        {
+            return this.numVotes;
+        }
+    }
+}
