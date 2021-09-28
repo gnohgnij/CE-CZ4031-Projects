9bb415d0cfd6521acc5a7c63c5dfa00f9a387b26
brygoh <72204360+brygoh@users.noreply.github.com>
2021-09-28 17:03:18 +0800

    scaled the b+tree

--- Project 1/Project 1/Insertion/BPlusTree.cs
+++ Project 1/Project 1/Insertion/BPlusTree.cs
@@ -1,104 +1,202 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Schema;
 
 namespace Project_1
 {
     public struct BPlusTree
     {
+        /*
+        * Instantiation
+        */
+
         private List<NonLeafNode> nonLeafNodes;
         private List<LeafNode> leafNodes;
 
+        /* 
+        * Insert Function
+        */
+
         public void insert(int numVotes, Record record)
         {
-            if (leafNodes == null)  //when BPlusTree is empty
+            /*
+            * When LeafNode is empty
+            */
+            if (leafNodes == null) 
             {
-                Console.WriteLine("leafNodes == null");
+                Console.WriteLine("LeafNodes == null");
+
                 this.leafNodes = new List<LeafNode>();
                 leafNodes.Add(new LeafNode(new List<int>(), new List<Record>(), 1));
+
                 leafNodes[0].getKeys().Add(numVotes);
                 leafNodes[0].getPointers().Add(record);
                 Console.WriteLine("LeafNodes[0]: ");
                 leafNodes[0].printAllKeys();
                 Console.WriteLine("--------------");
             }
-
             else
             {
-                int currIndex = this.leafNodes.Count - 1;   //need to change this when searching for which leafnode to insert to
-                if (leafNodes[currIndex].getKeys().Count == leafNodes[0].getMaxNumberOfKeys())  //when current leaf node is at max capacity
+                int[] indexIJ = search(numVotes);
+                int nodeIndex = indexIJ[0];  
+                int leafNodeIndex = indexIJ[1];
+
+                /*
+                * When LeafNode reaches max capacity
+                */
+                if (leafNodes[nodeIndex].getKeys().Count == leafNodes[0].getMaxNumberOfKeys())  
                 {
                     List<int> keyArr = new List<int>();
                     List<Record> recordArr = new List<Record>();
                     
-                    foreach (int i in leafNodes[currIndex].getKeys())
+                    foreach (int i in leafNodes[nodeIndex].getKeys())
                     {
                         keyArr.Add(i);
                     }
-
-                    foreach (Record r in leafNodes[currIndex].getPointers())
+                    foreach (Record r in leafNodes[nodeIndex].getPointers())
                     {
                         recordArr.Add(r);
                     }
-                    leafNodes[currIndex].getKeys().Clear();
-                    leafNodes[currIndex].getPointers().Clear();
-                    
+
+                    leafNodes[nodeIndex].getKeys().Clear();
+                    leafNodes[nodeIndex].getPointers().Clear();
                     keyArr.Add(numVotes);
                     recordArr.Add(record);
-                    
                     keyArr.Sort((k1, k2) => k1.CompareTo(k2));
                     recordArr.Sort((r1, r2) => r1.getNumVotes().CompareTo(r2.getNumVotes()));
                     
-                    for (int i = 0; i < (leafNodes[currIndex].getMaxNumberOfKeys() / 2) + 1; i++)
+                    for (int i = 0; i < (leafNodes[nodeIndex].getMaxNumberOfKeys() / 2) + 1; i++)
                     {
-                        leafNodes[currIndex].getKeys().Add(keyArr[i]);
-                        leafNodes[currIndex].getPointers().Add(recordArr[i]);
+                        leafNodes[nodeIndex].getKeys().Add(keyArr[i]);
+                        leafNodes[nodeIndex].getPointers().Add(recordArr[i]);
                     }
-                    Console.WriteLine("LeafNode[{0}]: ", currIndex);
-                    leafNodes[currIndex].printAllKeys();
-                    leafNodes[currIndex].printAllRecords();
+                    Console.WriteLine("LeafNode[{0}]: ", nodeIndex);
+                    leafNodes[nodeIndex].printAllKeys();
+                    leafNodes[nodeIndex].printAllRecords();
                     Console.WriteLine("--------------");
-                    
                     leafNodes.Add(new LeafNode(new List<int>(), new List<Record>(), 1));
-                    increaseLevel(this.leafNodes);
-
-                    this.nonLeafNodes = new List<NonLeafNode>();
-                    nonLeafNodes.Add(new NonLeafNode(1));
-                    nonLeafNodes[0].setKeys(new List<int>());
-
-                    currIndex++;
-                    for (int i = (leafNodes[currIndex].getMaxNumberOfKeys() / 2) + 1;
-                        i < leafNodes[currIndex].getMaxNumberOfKeys();
-                        i++)
+                    nodeIndex++;
+                    if (leafNodes[nodeIndex].getKeys() == null)
                     {
-                        leafNodes[currIndex].getKeys().Add(keyArr[i]);
-                        leafNodes[currIndex].getPointers().Add(recordArr[i]);
+                        for (int i = (leafNodes[nodeIndex].getMaxNumberOfKeys() / 2) + 1;
+                        i < leafNodes[nodeIndex].getMaxNumberOfKeys();
+                        i++)
+                        {
+                            leafNodes[nodeIndex].getKeys().Add(keyArr[i]);
+                            leafNodes[nodeIndex].getPointers().Add(recordArr[i]);
+                        }
+                        leafNodes[nodeIndex].getKeys().Add(keyArr[keyArr.Count-1]);
+                        leafNodes[nodeIndex].getPointers().Add(recordArr[recordArr.Count-1]);
+                        Console.WriteLine("LeafNode[{0}]: ", nodeIndex);
+                        leafNodes[nodeIndex].printAllKeys();
+                        leafNodes[nodeIndex].printAllRecords();
+                        Console.WriteLine("--------------");
                     }
-                    leafNodes[currIndex].getKeys().Add(keyArr[keyArr.Count-1]);
-                    leafNodes[currIndex].getPointers().Add(recordArr[recordArr.Count-1]);
+                    else 
+                    {
+                        nodeIndex++;
+                        leafNodes.Add(new LeafNode(new List<int>(), new List<Record>(), 1));
+                        for (int i = 0; i < leafNodes[nodeIndex-1].getKeys().Count; i++)
+                        {
+                            leafNodes[nodeIndex].getKeys().Add(leafNodes[nodeIndex-1].getKeys()[i]);
+                            leafNodes[nodeIndex].getPointers().Add(leafNodes[nodeIndex-1].getPointers()[i]);
+                        }
+                        Console.WriteLine("LeafNode[{0}]: ", nodeIndex);
+                        leafNodes[nodeIndex].printAllKeys();
+                        leafNodes[nodeIndex].printAllRecords();
+                        Console.WriteLine("--------------");
+                        leafNodes[nodeIndex-1].getKeys().Clear();
+                        leafNodes[nodeIndex-1].getPointers().Clear();
+                        for (int i = (leafNodes[nodeIndex].getMaxNumberOfKeys() / 2) + 1;
+                            i < leafNodes[nodeIndex].getMaxNumberOfKeys();
+                            i++)
+                        {
+                            leafNodes[nodeIndex-1].getKeys().Add(keyArr[i]);
+                            leafNodes[nodeIndex-1].getPointers().Add(recordArr[i]);
+                        }
+                        leafNodes[nodeIndex-1].getKeys().Add(keyArr[keyArr.Count-1]);
+                        leafNodes[nodeIndex-1].getPointers().Add(recordArr[recordArr.Count-1]);
+                        Console.WriteLine("LeafNode[{0}]: ", nodeIndex-1);
+                        leafNodes[nodeIndex-1].printAllKeys();
+                        leafNodes[nodeIndex-1].printAllRecords();
+                        Console.WriteLine("--------------");
+                    }
 
-                    //nonLeafNodes[0].getKeys().Add(leafNodes[currIndex].getKeys()[0]);
+                    // this.nonLeafNodes = new List<NonLeafNode>();
+                    // nonLeafNodes.Add(new NonLeafNode(new List<int>(), new List<LeafNode>(), new List<NonLeafNode>(), 1));
 
-                    Console.WriteLine("LeafNode[{0}]: ", currIndex);
-                    leafNodes[currIndex].printAllKeys();
-                    leafNodes[currIndex].printAllRecords();
+                    // for (int i = (leafNodes[nodeIndex].getMaxNumberOfKeys() / 2) + 1;
+                    //     i < leafNodes[nodeIndex].getMaxNumberOfKeys();
+                    //     i++)
+                    // {
+                    //     leafNodes[nodeIndex].getKeys().Add(keyArr[i]);
+                    //     leafNodes[nodeIndex].getPointers().Add(recordArr[i]);
+                    // }
+                    // leafNodes[nodeIndex].getKeys().Add(keyArr[keyArr.Count-1]);
+                    // leafNodes[nodeIndex].getPointers().Add(recordArr[recordArr.Count-1]);
+
+                    // nonLeafNodes[0].getKeys().Add(leafNodes[currIndex].getKeys()[0]);
+
+                    Console.WriteLine("LeafNode[{0}]: ", nodeIndex);
+                    leafNodes[nodeIndex].printAllKeys();
+                    leafNodes[nodeIndex].printAllRecords();
                     Console.WriteLine("--------------");
-
-                    //nonLeafNodes[0].printAllKeys();
-
-
+                    // nonLeafNodes[0].printAllKeys();
                 }
-
-                else //when leafnode still has spaces
+                /*
+                * When LeafNode has empty space
+                */
+                else 
                 {
-                    leafNodes[currIndex].getKeys().Add(numVotes);
-                    leafNodes[currIndex].getPointers().Add(record);
-                    Console.WriteLine("LeafNodes[{0}]: ", currIndex);
-                    leafNodes[currIndex].printAllKeys();
+                    if (leafNodeIndex == 0) {
+                        List<int> tempNumVotes = new List<int>();
+                        List<Record> tempRecords = new List<Record>();
+                        tempNumVotes.Add(numVotes);
+                        tempRecords.Add(record);
+
+                        foreach (int i in leafNodes[nodeIndex].getKeys())
+                        {
+                            tempNumVotes.Add(i);
+                        }
+                        foreach (Record r in leafNodes[nodeIndex].getPointers())
+                        {
+                            tempRecords.Add(r);
+                        }
+                        leafNodes[nodeIndex].getKeys().Clear();
+                        leafNodes[nodeIndex].getPointers().Clear();
+                    }
+
+                    leafNodes[nodeIndex].getKeys().Add(numVotes);
+                    leafNodes[nodeIndex].getPointers().Add(record);
+                    Console.WriteLine("LeafNodes[{0}]: ", nodeIndex);
+                    leafNodes[nodeIndex].printAllKeys();
+                    leafNodes[nodeIndex].printAllRecords();
                     Console.WriteLine("--------------");
                 }
             }
+        }
+         
+        public int[] search(int numVotes) 
+        {
+            if (nonLeafNodes == null) 
+            {
+                for (int i = 0; i < leafNodes.Count; i++) 
+                {
+                    for (int j = 0; j < leafNodes[i].getKeys().Count; j++)
+                    {
+                        if (numVotes >= leafNodes[i].getKeys()[j])
+                        {
+                            return new int[] {i, j+1};
+                        }
+                    }
+                }
+                return new int[] {0, 0};
+            }
+            return new int[] {0, 0};
+            // else
+            // {
+
+            // }
         }
 
         public void increaseLevel(List<LeafNode> leafNodes)
--- Project 1/Project 1/Node/NonLeafNode.cs
+++ Project 1/Project 1/Node/NonLeafNode.cs
@@ -11,11 +11,11 @@ namespace Project_1
         private static int maxNumberOfKeys = 3;
         private int level;
 
-        public NonLeafNode(int level)
+        public NonLeafNode(List<int> keys, List<LeafNode> leafNodes, List<NonLeafNode> nonLeafNodes, int level)
         {
-            keys = null;
-            nonLeafNodes = null;
-            leafNodes = null;
+            this.keys = keys;
+            this.leafNodes = leafNodes;
+            this.nonLeafNodes = nonLeafNodes;
             this.level = level;
         }
         public int getMaxNumberOfKeys()
@@ -28,47 +28,49 @@ namespace Project_1
             return keys;
         }
         
-        public void setKeys(List<int> keys)
-        {
-            this.keys = keys;
-        }
+        // public void setKeys(List<int> keys)
+        // {
+        //     this.keys = keys;
+        // }
 
         public List<NonLeafNode> getNonLeafNodes()
         {
             return this.nonLeafNodes;
         }
         
-        public void setNonLeafNodes(List<NonLeafNode> nonLeafNodes)
-        {
-            this.nonLeafNodes = nonLeafNodes;
-        }
+        // public void setNonLeafNodes(List<NonLeafNode> nonLeafNodes)
+        // {
+        //     this.nonLeafNodes = nonLeafNodes;
+        // }
         
         public List<LeafNode> getLeafNodes()
         {
             return this.leafNodes;
         }
         
-        public void setLeafNodes(List<LeafNode> leafNodes)
-        {
-            this.leafNodes = leafNodes;
-        }
+        // public void setLeafNodes(List<LeafNode> leafNodes)
+        // {
+        //     this.leafNodes = leafNodes;
+        // }
         
         public int getLevel()
         {
             return this.level;
         }
         
-        public void setLevel(int level)
-        {
-            this.level = level;
-        }
-
-        // public void printAllKeys()
+        // public void setLevel(int level)
         // {
-        //foreach (int i in keys)
-        // {
-        // Console.WriteLine("key = " + i);
-        //}
+        //     this.level = level;
         // }
+
+        public void printAllKeys()
+        {
+            int count = -1;
+            foreach (int i in keys)
+            {
+                count++;
+                Console.WriteLine("NonLeafNode[{0}] = " + i, count);
+            }
+        }
     }
 }
